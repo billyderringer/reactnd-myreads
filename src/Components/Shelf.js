cff92d691b78493b6c1be399b95ab95fa6eab1a6
Billy Derringer <billyderringer7@gmail.com>
2020-08-11 10:33:16 -0500

    Refactor book shelves to components
    Make each shelf a component with book data passed in as prop

--- src/Components/Shelf.js
+++ src/Components/Shelf.js
@@ -2,8 +2,49 @@ import React from "react"
 
 function Shelf(props){
     return(
-      <div>
-        Shelf {props.text}
+      <div className="bookshelf">
+        <h2 className="bookshelf-title">{props.shelfTitle}</h2>
+        <div className="bookshelf-books">
+          <ol className="books-grid">
+            {props.books && props.books.filter(book => book.shelf === props.filter).map(book => (
+              <li>
+              <div className="book">
+                <div className="book-top">
+                  <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url(${book.imageLinks.thumbnail})` }}></div>
+                  <div className="book-shelf-changer">
+                    <select>
+                      <option value="move" disabled>Move to...</option>
+                      <option
+                        selected={book.shelf === "currentlyReading"}
+                        value="currentlyReading">
+                        Currently Reading
+                      </option>
+                      <option
+                        selected={book.shelf === "wantToRead"}
+                        value="wantToRead">
+                        Want to Read
+                      </option>
+                      <option
+                        selected={book.shelf === "read"}
+                        value="read">
+                        Read
+                      </option>
+                      <option
+                        value="none">
+                        None
+                      </option>
+                    </select>
+                  </div>
+                </div>
+                <div className="book-title">{book.title}</div>
+                <div className="book-authors">{book.authors.map(author => (
+                  <div>{author}</div>
+                ))}</div>
+              </div>
+            </li>
+            ))}
+          </ol>
+        </div>
       </div>
     )
 }
